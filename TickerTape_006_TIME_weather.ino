26c99df52a00574c5c5bdd94b86b2d746ec4c094
Darren Dignam <darren.dignam@gmail.com>
2019-05-03 12:34:53 +0200

    Update TickerTape_006_TIME_weather.ino

--- TickerTape_006_TIME_weather.ino
+++ TickerTape_006_TIME_weather.ino
@@ -1,6 +1,6 @@
 /* ***************************************************************************************************** *\
 **     TickerTape Firmware                                                                               **
-**     (Kickstarter Backer Version 0.3)                                                                  **
+**     (Kickstarter Backer Version 0.6)                                                                  **
 **     (c) 2019 Darren Dignam                                                                            **
 **                                                                                                       **
 **                                                                                                       **
